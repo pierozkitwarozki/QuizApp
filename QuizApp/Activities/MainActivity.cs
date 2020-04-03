5ed7a16716c2ff240715dbe8485de503f9c8071b
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-04-03 19:51:05 +0200

    Dodaj pliki projektów.

---
+++ QuizApp/Activities/MainActivity.cs
@@ -0,0 +1,95 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace QuizApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false, NoHistory = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Android.Support.V7.Widget.Toolbar toolbar;
+        LinearLayout historyLayout;
+        LinearLayout geographyLayout;
+        LinearLayout spaceLayout;
+        LinearLayout engineeringLayout;
+        LinearLayout programmingLayout;
+        LinearLayout businessLayout;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+
+            //Setup toolbar
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "Topics";
+            Android.Support.V7.App.ActionBar actionBar = SupportActionBar;
+            actionBar.SetHomeAsUpIndicator(Resource.Drawable.menuaction);
+            actionBar.SetDisplayHomeAsUpEnabled(true);
+
+            //View Setups
+            historyLayout = FindViewById<LinearLayout>(Resource.Id.historyLayout);
+            geographyLayout = FindViewById<LinearLayout>(Resource.Id.geographyLayout);
+            spaceLayout = FindViewById<LinearLayout>(Resource.Id.spaceLayout);
+            engineeringLayout = FindViewById<LinearLayout>(Resource.Id.engineeringLayout);
+            programmingLayout = FindViewById<LinearLayout>(Resource.Id.programmingLayout);
+            businessLayout = FindViewById<LinearLayout>(Resource.Id.businessLayout);
+
+            //Click Event Handlers
+            historyLayout.Click += HistoryLayout_Click;
+            geographyLayout.Click += GeographyLayout_Click;
+            spaceLayout.Click += SpaceLayout_Click;
+            engineeringLayout.Click += EngineeringLayout_Click;
+            programmingLayout.Click += ProgrammingLayout_Click;
+            businessLayout.Click += BusinessLayout_Click;
+
+        }
+
+        private void BusinessLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Business");
+            StartActivity(intent);
+        }
+
+        private void ProgrammingLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Programming");
+            StartActivity(intent);
+        }
+
+        private void EngineeringLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Engineering");
+            StartActivity(intent);
+        }
+
+        private void SpaceLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Space");
+            StartActivity(intent);
+        }
+
+        private void GeographyLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Geography");
+            StartActivity(intent);
+        }
+
+        private void HistoryLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "History");
+            StartActivity(intent);
+        }
+    }
+}
---
+++ QuizApp/Activities/QuizDescriptionActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace QuizApp
+{
+    [Activity(Label = "QuizDescriptionActivity", Theme = "@style/AppTheme")]
+    public class QuizDescriptionActivity : AppCompatActivity
+    {
+        TextView topicTextView;
+        TextView topicDescriptionTextView;
+
+        ImageView quizImage;
+
+        Button startQuizButton;
+
+        string quizTopic;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_selected_topic);
+
+            //View Setups
+            topicTextView = FindViewById<TextView>(Resource.Id.topicTextView);
+            topicDescriptionTextView = FindViewById<TextView>(Resource.Id.topicDescriptionTextView);
+            quizImage = FindViewById<ImageView>(Resource.Id.quizImage);
+            startQuizButton = FindViewById<Button>(Resource.Id.startQuizButton);
+
+            //Get string
+            quizTopic = Intent.GetStringExtra("topic");
+            topicTextView.Text = quizTopic;
+
+            //Get Topic Image
+            quizImage.SetImageResource(GetImage(quizTopic));
+        }
+
+        private int GetImage(string topic)
+        {
+            int imageInt = 0;
+            if(topic == "History")
+            {
+                imageInt = Resource.Drawable.history;
+            }
+            else if(topic == "Geography")
+            {
+                imageInt = Resource.Drawable.geography;
+            }
+            else if (topic == "Space")
+            {
+                imageInt = Resource.Drawable.space;
+            }
+            else if (topic == "Engineering")
+            {
+                imageInt = Resource.Drawable.engineering;
+            }
+            else if (topic == "Programming")
+            {
+                imageInt = Resource.Drawable.programming;
+            }
+            else if (topic == "Business")
+            {
+                imageInt = Resource.Drawable.business;
+            }
+            return imageInt;
+        }
+    }
+}
